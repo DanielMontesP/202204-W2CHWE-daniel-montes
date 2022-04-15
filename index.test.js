db3c7d9b21bdd167e2255ed8f9edbf6e5d6067ff
Daniel Montes <dmp@MacBook-Pro-de-Daniel.local>
2022-04-15 22:48:16 +0200

    Add git Actions conf

---
+++ index.test.js
@@ -0,0 +1,16 @@
+function begin() {
+  return "begin";
+}
+
+describe("Given a class begin", () => {
+  //   describe("When it receives 'begin'", () => {
+  //     test("Then it should return 'begin'", () => {
+  //       // const text = "begin";
+  //       const expectedText = "begin";
+  //       const resultText = begin();
+  //       expected(resultText.toBe(expectedText));
+  //     });
+  //   });
+});
+
+console.log(begin());
